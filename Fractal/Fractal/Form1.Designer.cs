59b003f4f81435694b1b9cca2ca2ae77b9e10a88
Alexandr <rubtsov.a.p@edu.mirea.ru>
2021-05-24 19:38:27 +0300

    фрактал

---
+++ Fractal/Fractal/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Fractal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(736, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 144);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start from begining";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(741, 280);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 39);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Black-White";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(740, 341);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 37);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Yellow";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(739, 399);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 37);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Red-Blue-Black";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(741, 457);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(110, 36);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Blue-Black";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(883, 280);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(76, 38);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Classic";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Fractal";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Fractal/Fractal/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fractal {
+    public partial class Form1 : Form {
+        public class Complex {
+            private double r, i;
+            public void setR(double r) {
+                this.r = r;
+            }
+
+            public double getR() {
+                return this.r;
+            }
+
+            public void setI(double i) {
+                this.i = i;
+            }
+
+            public double getI() {
+                return this.i;
+            }
+
+                public Complex() {
+                this.r = 0.0;
+                this.i = 0.0;
+            }
+            public void Add(Complex b) {
+                this.r += b.getR();
+                this.i += b.getI();
+            }
+            public static Complex Sum(Complex a, Complex b) {
+                Complex res = new Complex();
+                res.setR(a.getR() + b.getR());
+                res.setI(a.getI() + b.getI());
+                return res;
+            }
+            public static Complex Multiplication(Complex a, Complex b) {
+                Complex res = new Complex();
+                res.setR(a.getR() * b.getR() - a.getI() * b.getI());
+                res.setI(a.getI() * b.getR() + a.getR() * b.getI());
+                return res;
+            }
+            public static Complex Subtract(Complex a, Complex b)
+            {
+                Complex res = new Complex();
+                res.setR(a.getR() - b.getR());
+                res.setI(a.getI() - b.getI());
+                return res;
+            }
+            public static Complex operator +(Complex a, Complex b)
+            {
+                return Complex.Sum(a, b);
+            }
+            public static Complex operator -(Complex a, Complex b)
+            {
+                return Complex.Subtract(a, b);
+            }
+            public static Complex operator *(Complex a, Complex b)
+            {
+                return Complex.Multiplication(a, b);
+            }
+        }
+        double inff = 2.0;
+        int color_cnt = 5; 
+        double xl = -2.5;
+        double xr = 1.5;
+        double yu = 2;
+        double yd = -2;
+        int iter = 255;
+        Complex inf = new Complex();
+        int get_cnt(Complex c) {
+            inf.setR(inff);
+            Complex z = new Complex();
+            int i = 0;
+            while ((z.getR() * z.getR() + z.getI() * z.getI()) < (inf.getR() * inf.getR()) && i < iter) {
+                z = z*z;
+                z.Add(c);
+                i++;
+            }
+            return i;
+        }
+        Color get_color1(int cnt) {
+            return Color.FromArgb(((255 - cnt) * cnt)%256, ((255 - cnt) * cnt)%256, ((255 - cnt) * cnt)%256);
+        }
+        Color get_color2(int cnt) {
+            return Color.FromArgb(Math.Min((255 - cnt), 255), ((255 - cnt) * cnt)%256, (((255 - cnt) * 255) % 255*cnt)%256);
+        }
+        Color get_color3(int cnt) {
+            return Color.FromArgb((( cnt) * cnt) % 256, (((255 - cnt) * 255) % 255 * cnt) % 256, Math.Min((cnt), 255));
+        }
+        Color get_color4(int cnt) {
+            return Color.FromArgb((((255 - cnt) * 255) % 255 * cnt) % 256, ((255 - cnt) * cnt) % 256, Math.Min((255 - cnt), 255));
+        }
+        Color get_color5(int cnt) {
+            return Color.FromArgb(255 - cnt, 255 - cnt, 255 - cnt);
+        }
+        public Form1() {
+            InitializeComponent();
+        }
+        void Draw_Picture() {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (var g = Graphics.FromImage(bmp))
+            {
+                double dx = (xr - xl) / pictureBox1.Width;
+                double dy = (yu - yd) / pictureBox1.Height;
+                for (int i = 0; i < pictureBox1.Height; ++i)
+                {
+                    for (int j = 0; j < pictureBox1.Width; ++j)
+                    {
+                        Complex c = new Complex();
+                        c.setR(xl + j * dx);
+                        c.setI(yu - i * dy);
+                        if (color_cnt == 1)
+                            bmp.SetPixel(j, i, get_color1(get_cnt(c)));
+                        if (color_cnt == 2)
+                            bmp.SetPixel(j, i, get_color2(get_cnt(c)));
+                        if (color_cnt == 3)
+                            bmp.SetPixel(j, i, get_color3(get_cnt(c)));
+                        if (color_cnt == 4)
+                            bmp.SetPixel(j, i, get_color4(get_cnt(c)));
+                        if(color_cnt == 5)
+                            bmp.SetPixel(j, i, get_color5(get_cnt(c)));
+                    }
+                }
+                pictureBox1.BackgroundImage = bmp;
+                pictureBox1.Image = null;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e) {
+            xl = -2.5;
+            xr = 1.5;
+            yu = 2;
+            yd = -2;
+            Draw_Picture();
+        }
+        int XN = 0;
+        int YN = 0;
+        bool flag = false;
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e) {
+            XN = e.X;
+            YN = e.Y;
+            flag = true;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e) {
+            int X = e.X;
+            int Y = e.Y;
+            double dx = (xr - xl) / pictureBox1.Width;
+            double dy = (yu - yd) / pictureBox1.Height;
+            if (XN > X) {
+                int t = XN;
+                XN = X;
+                X = t;
+            }
+            if(Y > YN) {
+                int t = YN;
+                YN = Y;
+                Y = t;
+            }
+            if(X - XN > YN - Y) {
+                Y += (X - XN) - (YN - Y);
+            }
+            else {
+                X += (YN - Y) - (X - XN);
+            }
+            xr = xl + X * dx;
+            xl = xl + XN * dx;
+            yd = yu - YN * dy;
+            yu = yu - Y * dy;
+            flag = false;
+            Draw_Picture();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            color_cnt = 1;
+            Draw_Picture();
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            color_cnt = 2;
+            Draw_Picture();
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            color_cnt = 3;
+            Draw_Picture();
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+            color_cnt = 4;
+            Draw_Picture();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e) {
+            if(flag) {
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                using (var g = Graphics.FromImage(bmp)) {
+                    Pen p = new Pen(Color.Red, 2);
+                    p.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+                    g.DrawRectangle(p, Math.Min(XN, e.X), Math.Min(YN, e.Y), Math.Abs(XN - e.X), Math.Abs(YN - e.Y));
+                    pictureBox1.Image = bmp;
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e) {
+            color_cnt = 5;
+            Draw_Picture();
+        }
+    }
+}
